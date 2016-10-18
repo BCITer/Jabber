18a6de96441cbc929d2685a2bc748046a51ece48
Jeremy Yang <yy25036542@gmail.com>
2016-10-18 12:16:36 -0700

    Created a new controller for the forum page. Added a method to load FormMain view.

---
+++ JabberBCIT/JabberBCIT/Controllers/ForumController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JabberBCIT.Controllers
+{
+    public class ForumController : Controller
+    {
+        // GET: Forum
+        public ActionResult ForumMain()
+        {
+            return View();
+        }
+    }
+}
