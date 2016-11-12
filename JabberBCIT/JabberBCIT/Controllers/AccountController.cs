7a65a9526da77b5523aa612fcb836a03342f9557
Xavier Lau <xavierlau1@gmail.com>
2016-11-12 01:39:33 -0800

    Change login to use email sign in

--- JabberBCIT/JabberBCIT/Controllers/AccountController.cs
+++ JabberBCIT/JabberBCIT/Controllers/AccountController.cs
@@ -88,7 +88,7 @@ namespace JabberBCIT.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordEmailSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
