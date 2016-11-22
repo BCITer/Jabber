1db2d072929695991302e455f597bfe2b1c21042
manish mallavarapu <manish101096@gmail.com>
2016-11-22 13:13:49 -0800

    adding google drive authentication
    Signed-off-by: manish mallavarapu <manish101096@gmail.com>

---
+++ JabberBCIT/JabberBCIT/App_Code/GoogleDriveAuthenticator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace JabberBCIT.App_Code
+{
+    public class GoogleDriveAuthenticator
+    {
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string ApplicationName = "Drive API .NET Quickstart";
+        private GoogleDriveAuthenticator()
+        {
+
+        }
+    }
+}
