b22ed26578556e12e9eb6c7dfa30a8d137cd715a
Lei Zhang(Daniel) <danielvmaple@gmail.com>
2016-11-29 11:11:13 -0800

    Done with preview profile

---
+++ JabberBCIT/JabberBCIT/Content/preview.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#Avatar').on("change", function () {
+        var file = this.files[0];
+
+        if (this.files && file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#profile-picture').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(file);
+        }
+    });
+});
