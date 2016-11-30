0903ae76e4f07a736001f5c1431c05121fa4bc12
Justin Huchet <justin.huchet@gmail.com>
2016-11-30 11:15:52 -0800

    Paramaterized queries
    Signed-off-by: Justin Huchet <justin.huchet@gmail.com>

--- JabberBCIT/JabberBCIT/Controllers/ChatController.cs
+++ JabberBCIT/JabberBCIT/Controllers/ChatController.cs
@@ -17,93 +17,49 @@ namespace JabberBCIT.Controllers
         // GET: Chat
         public ActionResult Chat(ChatGroup cg)
         {
-            string groupName = cg.GroupName;
-            string[] members = cg.Members;
-
-            DataTable dtNames = new DataTable();
-
-            string sqlQuery = "insert into ChatConversation (\"ChatName\", \"UserID\") values ('" + cg.GroupName + "', '" + User.Identity.GetUserId() + "')";
-
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ChitterContext"].ConnectionString;
-
-            try
+            if (!String.IsNullOrEmpty(cg.GroupName))
             {
+                string groupName = cg.GroupName;
+                string[] members = cg.Members;
+                int chatID;
+
+                //Create the new chat
+                string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ChitterContext"].ConnectionString;
                 SqlConnection conn = new SqlConnection(connectionString);
-                SqlDataAdapter da = new SqlDataAdapter(sqlQuery, conn);
+                conn.Open();
 
-                da.Fill(dtNames);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            
-            string chatID;
+                string sqlQuery = "insert into ChatConversation (ChatName, UserID) values (@Name, @User)";
 
-            using (SqlConnection con = new SqlConnection())
-            {
-                con.ConnectionString = ConfigurationManager.ConnectionStrings["ChitterContext"].ConnectionString;
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+                cmd.Parameters.AddWithValue("@Name", cg.GroupName);
+                cmd.Parameters.AddWithValue("@User", User.Identity.GetUserId());
 
-                using (SqlCommand com = new SqlCommand())
+                cmd.ExecuteNonQuery();
+
+                //Get the new ChatID
+
+                sqlQuery = "select ChatID from ChatConversation a left join (select UserID, max(\"Timestamp\") as \"Max\" from ChatConversation group by UserID) b on a.UserID=b.UserID where a.\"Timestamp\" = b.\"Max\" and a.UserID = @User";
+
+                cmd = new SqlCommand(sqlQuery, conn);
+                cmd.Parameters.AddWithValue("@User", User.Identity.GetUserId());
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
                 {
-                    com.CommandText = "select ChatID from ChatConversation a left join (select UserID, max(\"Timestamp\") as \"Max\" from ChatConversation group by UserID) b on a.UserID=b.UserID where a.\"Timestamp\" = b.\"Max\" and a.UserID = '" + User.Identity.GetUserId() + "'";
-                    
-                    com.Connection = con;
-                    con.Open();
-                    using (SqlDataReader sdr = com.ExecuteReader())
+                    if (reader.Read())
                     {
-                        while (sdr.Read())
-                        {
-                            chatID = sdr["ChatID"].ToString();
-                        }
+                        chatID = Int32.Parse(reader["ChatID"].ToString());
                     }
-
-                    con.Close();
                 }
+
+                conn.Close();
+
+                //Return the ID as well in the autocomplete
+                /* foreach(string member in cg.Members)
+                 {
+                     sqlQuery = "insert into ChatConversation (\"ChatName\", \"UserID\") values ('" + chatID + "', '" + User.Identity.GetUserId() + "')";
+                 }*/
             }
-
-
-            //Return the ID as well in the autocomplete
-           /* foreach(string member in cg.Members)
-            {
-                sqlQuery = "insert into ChatConversation (\"ChatName\", \"UserID\") values ('" + chatID + "', '" + User.Identity.GetUserId() + "')";
-            }*/
-
             return View(cg);
-        }
-
-        public ActionResult Autocomplete(string term)
-        {
-            List<string> users = new List<string>();
-            
-            DataTable dtNames = new DataTable();
-
-            string sqlQuery = "select UserName from AspNetUsers";
-
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ChitterContext"].ConnectionString;
-
-            try {
-                SqlConnection conn = new SqlConnection(connectionString);
-                SqlDataAdapter da = new SqlDataAdapter(sqlQuery, conn);
-
-                da.Fill(dtNames);
-
-                foreach (DataRow row in dtNames.Rows)
-                {
-                    string name = Convert.ToString(row["UserName"]);
-                    users.Add(name);
-                }
-            } catch (Exception ex) {
-                throw ex;
-            }
-
-            var filteredItems = users.Where(
-                item => item.IndexOf(term, StringComparison.InvariantCultureIgnoreCase) >= 0
-                );
-            return Json(filteredItems, JsonRequestBehavior.AllowGet);
-        }
-
-       
-    
+        }    
     }
 }
