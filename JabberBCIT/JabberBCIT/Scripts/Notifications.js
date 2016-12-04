06bfd2dff496f4600a50fb1a0712a8f9593408ea
Jason Burns <undbitr42@hotmail.com>
2016-12-03 23:45:36 -0800

    red icon gone

--- JabberBCIT/JabberBCIT/Scripts/Notifications.js
+++ JabberBCIT/JabberBCIT/Scripts/Notifications.js
@@ -1,10 +1,4 @@
 ﻿$(document).ready(function () {
-    // ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
-    $('#noti_Counter')
-        .css({ opacity: 0 })
-        .css({ top: '-10px' })
-        .animate({ top: '-2px', opacity: 1 }, 500);
-
     $('#noti_Button').click(function () {
         // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
         $('#notifications').fadeToggle('fast', 'linear', function () {
